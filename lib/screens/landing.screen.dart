789a307bba254d473c7d56acabb99d3314ac6524
valekar <srinivas.valekar@gmail.com>
2020-01-01 21:12:25 +0530

    added scanny plugin

--- lib/screens/landing.screen.dart
+++ lib/screens/landing.screen.dart
@@ -2,9 +2,51 @@ import '../widgets/item.widget.dart';
 
 import '../widgets/app_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:scanny/scanny.dart';
 
-class LandingScreen extends StatelessWidget {
+class LandingScreen extends StatefulWidget {
   static const routeName = "landing-screen";
+
+  @override
+  _LandingScreenState createState() => _LandingScreenState(new Scanny());
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  dynamic _imageBytes;
+  final Scanny scanny;
+
+  _LandingScreenState(this.scanny);
+
+  @override
+  void initState() {
+    super.initState();
+
+    () async {
+      //ask permissions
+      //askPermissions();
+    }();
+  }
+
+  Future<void> askPermissions() async {
+    scanny.askPermissions;
+  }
+
+  Future<void> scanDocument() async {
+    try {
+      //ask permissions if permissions are not granted yet
+      await askPermissions();
+      //call the scanner
+      scanny.callScanner;
+      //listen to the results of activity
+      scanny.getImageBytes.listen((imageBytes) {
+        setState(() {
+          _imageBytes = imageBytes;
+        });
+      });
+    } catch (error) {
+      print(error);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +59,11 @@ class LandingScreen extends StatelessWidget {
       drawer: AppDrawer(),
       body: Center(
         child: ItemWidget(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: scanDocument,
+        child: Icon(Icons.camera_enhance),
+        backgroundColor: Colors.teal,
       ),
     );
   }
