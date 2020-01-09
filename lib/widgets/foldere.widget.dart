31100ae28f1f756e2db854d5126d54acac2b43c5
Srinivas Valekar <srinivas.prashanth.valekar@sap.com>
2020-01-09 18:20:56 +0000

    folder UI adjustments

--- lib/widgets/foldere.widget.dart
+++ lib/widgets/foldere.widget.dart
@@ -1,9 +1,11 @@
+import '../models/model.dart' as model;
 import 'package:basic/screens/folder.detail.screen.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
+import 'package:intl/intl.dart';
 
 class FolderWidget extends StatelessWidget {
-  final folders;
+  final List<model.Folder> folders;
 
   FolderWidget(this.folders);
 
@@ -18,13 +20,29 @@ class FolderWidget extends StatelessWidget {
             width: double.infinity,
             padding: EdgeInsets.symmetric(vertical: 10, horizontal: 1),
             child: ListTile(
-              leading: Image.file(
-                File(folders[index].imageUrl),
-                fit: BoxFit.fill,
+              leading: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: 80,
+                  minWidth: 80,
+                  maxHeight: 80,
+                  maxWidth: 80,
+                ),
+                child: Image.file(
+                  File(folders[index].imageUrl),
+                  fit: BoxFit.fill,
+                ),
               ),
               title: Text(folders[index].title),
-              //subtitle: Text(_items.getItems[index].description),
+              subtitle: Text(
+                DateFormat('dd-MM-yyy').format(
+                  DateTime.fromMillisecondsSinceEpoch(folders[index].createdAt),
+                ),
+              ),
               selected: false,
+              trailing: IconButton(
+                icon: Icon(Icons.delete),
+                onPressed: () {},
+              ),
               onTap: () {
                 Navigator.of(context).pushNamed(FolderDetailScreen.routeName,
                     arguments: {"id": folders[index].id});
